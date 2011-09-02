fc2bd686b0281e0d8e4d2c089020ac1549ae6b06
Andrew Maurer <git@maurer.me>
2011-09-02 16:49:16 -0400

    Fixed few issues

--- approot/server.js
+++ approot/server.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var app = express.createServer();
 
-app.configure(function(){
-	app.set('view engine', 'jade');
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(app.router);
-	app.use(express.static(__dirname + '/assets'));
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+	app.configure(function(){
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'jade');
+		app.use(express.methodOverride());
+		app.use(express.bodyParser());
+		app.use(app.router);
+		app.use(express.static(__dirname + '/assets'));
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
 
-app.get('/', function(req, res){
-	res.render('index.jade');
-});
+	app.get('/', function(req, res){
+		res.render('index.jade', {
+			layout : 'layouts/single_col_full.jade',
+			title : 'Is cool'
+		});
+	});
 
-app.listen(3000);
+	app.listen(3000);
